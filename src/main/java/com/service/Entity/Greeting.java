2f1a8186748a2d080de65b8533a8c52576039677
Amboara <andriamalalaamboarafaneva@gmail.com>
2026-03-12 22:32:53 +0300

    push final tutorial

---
+++ src/main/java/com/service/Entity/Greeting.java
@@ -0,0 +1,4 @@
+package com.service.Entity;
+
+public record Greeting(long id, String content) {
+}
--- src/test/java/tutorialspring/TutorialSpringApplicationTests.java
+++ src/test/java/tutorialspring/TutorialSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package tutorialspring.tutorialspring;
+package tutorialspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
